aac75b0187d4a979b43843e4bcdd2dc4777e5b4b
Arthur Dev <73297053+ArthurDeveloper@users.noreply.github.com>
2021-11-29 22:02:27 -0300

    Solved challenge!

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,13 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(array) do
+    call(array, 0)
+  end
+
+  defp call(array, current_length) do
+    if Enum.at(array, current_length) == nil do
+      current_length
+    else
+      call(array, current_length+1)
+    end
   end
 end
